93c3522f6857b0eac4575ab45190c9458788de0b
1458611 <116083529+1458611@users.noreply.github.com>
2022-10-18 13:03:53 +0000

    random gen

--- main.cs
+++ main.cs
@@ -6,7 +6,8 @@ class Program
   {
     //crate array for months of the year
     string[] months = new string[12] 
-    {"jan","feb","march","apr","apr","may","june","jul",""aug","sep","oct","nov","dec"};
+    {"jan","feb","march","apr","apr","may","june","jul","aug","sep","oct","nov"};
+    //initialise prng with time-dependent default seed value random rnd = new random();
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
   }
