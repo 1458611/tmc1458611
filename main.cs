e25ed211f8cb752aae8022af9d596d7d0ef51339
1458611 <116083529+1458611@users.noreply.github.com>
2022-10-18 12:55:55 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program 
+{
+  static void main()
+  {
+    //crate array for months of the year
+    string[] months = new string[12] 
+    {"jan","feb","march","apr","apr","may","june","jul",""aug","sep","oct","nov","dec"};
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
